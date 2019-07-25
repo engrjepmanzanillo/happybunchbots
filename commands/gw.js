b4038ae25929d9307c470840b76b2de615393ae4
jeff <jep.manzanillo@gmail.com>
2019-07-25 17:15:17 +0800

    debugging

--- commands/gw.js
+++ commands/gw.js
@@ -6,21 +6,21 @@ module.exports = {
 	aliases: [ 'guildwar' ],
 	cooldown: 5,
 	execute(message, member, args) {
-		// if (!message.author.hasPermission('MANAGE_CHANNEL')) {
-		// 	message.reply("Sorry, you don't have a permission to use that");
-		// }
-		console.log(message.member.hasPermission('MANAGE_CHANNELS'));
+		if (!message.member.hasPermission('MANAGE_CHANNELS')) {
+			message.reply("Sorry, you don't have a permission to use that");
+		}
+		//console.log(message.member.hasPermission('MANAGE_CHANNELS'));
 
-		// if (!args.length) {
-		// 	message.reply('Please specify ON or OFF');
-		// }
-		// let settings = args[0].toUpperCase();
-		// if (settings === 'ON') {
-		// 	message.channel.send('Reminder for Guild War was set ON.');
-		// } else if (settings === 'OFF') {
-		// 	message.channel.send('Reminder for Guild War was set OFF.');
-		// } else {
-		// 	message.reply('Please specify ON or OFF');
-		// }
+		if (!args.length) {
+			message.reply('Please specify ON or OFF');
+		}
+		let settings = args[0].toUpperCase();
+		if (settings === 'ON') {
+			message.channel.send('Reminder for Guild War was set ON.');
+		} else if (settings === 'OFF') {
+			message.channel.send('Reminder for Guild War was set OFF.');
+		} else {
+			message.reply('Please specify ON or OFF');
+		}
 	}
 };
