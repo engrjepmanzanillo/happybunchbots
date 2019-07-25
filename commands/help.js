2398a8c792d8e8d2a91c3171da2128692f631034
jeff <jep.manzanillo@gmail.com>
2019-07-25 11:46:26 +0800

    fixed

--- commands/help.js
+++ commands/help.js
@@ -11,33 +11,36 @@ module.exports = {
 		const { commands } = message.client;
 
 		if (!args.length) {
-			const name = args[0].toLowerCase();
-			const command = commands.get(name) || commands.find((c) => c.aliases && c.aliases.includes(name));
+			data.push("Here's a list of all available commands:");
+			data.push(commands.map((command) => command.name).join(', '));
+			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-			if (!command) {
-				return message.reply("that's not a valid command!");
-			}
+			return message.author
+				.send(data, { split: true })
+				.then(() => {
+					if (message.channel.type === 'dm') return;
+					message.reply("I've sent you a DM with all my commands!");
+				})
+				.catch((error) => {
+					console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+					message.reply("it seems like I can't DM you! Please enable DMs.");
+				});
+		}
+		const name = args[0].toLowerCase();
+		const command = commands.get(name) || commands.find((c) => c.aliases && c.aliases.includes(name));
 
-			data.push(`**Name:** ${command.name}`);
-
-			if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-			if (command.description) data.push(`**Description:** ${command.description}`);
-			if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+		if (!command) {
+			return message.reply("that's not a valid command!");
 		}
 
-		data.push("Here's a list of all available commands:");
-		data.push(commands.map((command) => command.name).join(', '));
-		data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+		data.push(`**Name:** ${command.name}`);
 
-		return message.author
-			.send(data, { split: true })
-			.then(() => {
-				if (message.channel.type === 'dm') return;
-				message.reply("I've sent you a DM with all my commands!");
-			})
-			.catch((error) => {
-				console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-				message.reply("it seems like I can't DM you! Please enable DMs.");
-			});
+		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+		if (command.description) data.push(`**Description:** ${command.description}`);
+		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+
+		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+
+		message.channel.send(data, { split: true });
 	}
 };
